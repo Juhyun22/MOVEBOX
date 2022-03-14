1d1b149cea9b81a05390de57c89bf1f7147ffa42
Juhyun-Bak <sihyun2533@gmail.com>
2022-03-14 18:00:44 +0900

    setting

---
+++ src/main/java/com/movebox/MoveboxApplication.java
@@ -0,0 +1,13 @@
+package com.movebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movebox/MoveboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
